86d8093df91bff8a83dbe5baadf0a664109ad8fb
BenBinary <43401683+BenBinary@users.noreply.github.com>
2021-10-13 15:23:04 +0200

    first attempt to take the photo

--- Readme.md
+++ Readme.md
@@ -1,4 +1,8 @@
 ### Picture Saving App
 
+#### Intension
 These App is just intended for taking, saving and processing images on Android.
 
+#### Structure Approach and Technology
+
+w
--- app/src/main/java/com/example/android_picture_saving/MainActivity.java
+++ app/src/main/java/com/example/android_picture_saving/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android_picture_saving;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,18 +21,23 @@ import com.example.android_picture_saving.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RESULT_OK = 0;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         setSupportActionBar(binding.toolbar);
 
@@ -73,4 +82,33 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    public void startActivityForResult(Intent takePictureIntent, int requestImageCapture) {
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // imageView.setImageBitmap(imageBitmap);
+        }
+
+    }
+
 }
